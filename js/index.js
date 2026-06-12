ab174600a4ab340a9d7e969cdc0e590c505b88d1
Corentin <98367308+Nestradama@users.noreply.github.com>
2026-06-13 00:29:23 +0200

    feat: fetch data and populate gallery through API

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const API_URL = "http://localhost:5678/api"
+const GALLERY_CONTAINER = document.getElementById("gallery_container")
+
+
+
+async function getImages() {
+    try {
+        const response = await fetch(API_URL + "/works");
+
+        if (!response.ok) {
+            console.error(`Response status: ${response.status}`);
+            return [];
+        }
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+}
+
+function populateGallery(json){
+    if (!json || json.length === 0) return;
+    GALLERY_CONTAINER.innerHTML=''
+    for(let i = 0; i < json.length; i++){
+        let item = json[i]
+        const FIGURE_TEMPLATE = `
+        <figure>
+            <img src="${item.imageUrl}" alt="Abajour Tahina"/>
+            <figcaption>${item.title}</figcaption>
+        </figure>
+        `
+        GALLERY_CONTAINER.insertAdjacentHTML('beforeend', FIGURE_TEMPLATE)
+    }
+}
+
+
+async function init() {
+    const data = await getImages();
+    populateGallery(data);
+}
+
+init();
