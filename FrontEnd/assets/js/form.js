d5ba52e2240efe2160720b3648a0a6bdbc0d8af4
Corentin <98367308+Nestradama@users.noreply.github.com>
2026-06-14 21:17:29 +0200

    fix: #login_form types in login.html feat: added login logic

---
+++ FrontEnd/assets/js/form.js
@@ -0,0 +1,35 @@
+const API_URL = "http://localhost:5678/api"
+
+document.querySelector('#login_form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const userEmail = document.getElementById("email").value;
+    const userPassword = document.getElementById("password").value;
+
+    const loginData = {
+        email: userEmail,
+        password: userPassword
+    };
+
+    fetch(`${API_URL}/users/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Erreur dans l’identifiant ou le mot de passe");
+            }
+        })
+        .then(data => {
+            window.localStorage.setItem("token", data.token);
+            window.location.href = "../index.html";
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
